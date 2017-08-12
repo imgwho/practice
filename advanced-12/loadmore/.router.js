9bd17cce03608ed969ad47c656902039be844594
imgwho <imgwho@qq.com>
2017-08-12 16:46:14 +0800

    update: add ajax demo 3

--- advanced-12/loadmore/.router.js
+++ advanced-12/loadmore/.router.js
@@ -4,20 +4,19 @@ function setRouter(app){
 // 服务端 router.js
 
 
-app.get('/loadMore', function(req, res) {
-  
-    var curIdx = req.query.index
-    var len = req.query.length
-    var data = []
-  
-    for(var i = 0; i < len; i++) {
-      data.push('新闻' + (parseInt(curIdx) + i))
-    }
-  
-    setTimeout(function(){
-      res.send(data);
-    }, 1000)
-    
-  });
+app.get('/loadMore', function (req, res) {
+
+  var curIdx = req.query.index
+  var len = req.query.length
+  var data = []
+
+  for (var i = 0; i < len; i++) {
+    data.push('新闻' + (parseInt(curIdx) + i))
   }
+
+  setTimeout(function () {
+    res.send(data)
+  }, 1000)
+
+})}
  module.exports.setRouter = setRouter
