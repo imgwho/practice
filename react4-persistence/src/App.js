789ffeb1259ff95048dd9cba1bc85d683717fc97
imgwho <imgwho@qq.com>
2017-08-30 01:45:09 +0800

    fix some bugs

--- react4-persistence/src/App.js
+++ react4-persistence/src/App.js
@@ -1,122 +1,115 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import 'normalize.css'
+import './reset.css'
 import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
-import 'normalize.css'
 import UserDialog from './UserDialog'
-
-import {getCurrentUser, signOut} from './leancloud'
+import {getCurrentUser, signOut, TodoModel} from './leancloud'
 
 class App extends Component {
-  constructor(props) {
+  constructor(props){
     super(props)
     this.state = {
       user: getCurrentUser() || {},
       newTodo: '',
       todoList: []
     }
+    let user = getCurrentUser()
+    if (user) {
+      TodoModel.getByUser(user, (todos) => {
+        let stateCopy = JSON.parse(JSON.stringify(this.state))
+        stateCopy.todoList = todos
+        this.setState(stateCopy)
+      })
+    }
   }
-
   render() {
+
     let todos = this.state.todoList
       .filter((item)=> !item.deleted)
-      .map((item, index)=>{
-
-      
-      // return <li>{item.title}</li>
-      return (
-        <li key={index}>
-          <TodoItem todo = {item} onToggle = {this.toggle.bind(this)}
-            onDelete = {this.delete.bind(this)}/>
+      .map((item,index)=>{
+      return ( // 为什么这里要加个括号？这是动手题3 🐸
+        <li key={index} >
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+            onDelete={this.delete.bind(this)}/>
         </li>
-
       )
     })
 
-
     return (
       <div className="App">
-         <h1>{this.state.user.username||'我'}的待办
-           {this.state.user.id ? <button onClick={this.signOut.bind(this)}>登出</button> : null}
-         </h1>
-        <div>
-          {/* <input type="text" value={title.state.newTodo}/> */}
+        <h1>{this.state.user.username||'我'}的待办
+          {this.state.user.id ? <button onClick={this.signOut.bind(this)}>登出</button> : null}
+        </h1>
+        <div className="inputWrapper">
           <TodoInput content={this.state.newTodo}
-            onChange = {this.changeTitle.bind(this)}
-            onSubmit = {this.addTodo.bind(this)} />
+            onChange={this.changeTitle.bind(this)}
+            onSubmit={this.addTodo.bind(this)} />
         </div>
-      <ol className="todoList">
-        {todos}
-      </ol>
-      {this.state.user.id ? 
-           null : 
-           <UserDialog 
-           onSignUp={this.onSignUpOrSignIn.bind(this)} 
-           onSignIn={this.onSignUpOrSignIn.bind(this)}/>}
+        <ol className="todoList">
+          {todos}
+        </ol>
+        {this.state.user.id ?
+          null :
+          <UserDialog
+            onSignUp={this.onSignUpOrSignIn.bind(this)}
+            onSignIn={this.onSignUpOrSignIn.bind(this)}/>}
       </div>
     )
   }
-
-
-  signOut() {
+  signOut(){
     signOut()
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.user = {}
     this.setState(stateCopy)
   }
-
-
-  onSignUpOrSignIn(user) {
+  onSignUpOrSignIn(user){
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.user = user
     this.setState(stateCopy)
   }
-
-
-  ComponentDidUpdate(){
+  componentDidUpdate(){
   }
-
-  toggle(e, todo) {
-    todo.status = todo.status === 'cpmpleted' ? '' : 'completed'
-    this.setState(this.state)
+  toggle(e, todo){
+    let oldStatus = todo.status
+    todo.status = todo.status === 'completed' ? '' : 'completed'
+    TodoModel.update(todo, () => {
+      this.setState(this.state)
+    }, (error) => {
+      todo.status = oldStatus
+      this.setState(this.state)
+    })
   }
-
-
-  changeTitle(event) {
+  changeTitle(event){
     this.setState({
       newTodo: event.target.value,
       todoList: this.state.todoList
     })
   }
-
-  addTodo(event) {
-    this.state.todoList.push({
-      id: idMaker(),
+  addTodo(event){
+    let newTodo = {
       title: event.target.value,
-      status: null,
+      status: '',
       deleted: false
-    })
-    this.setState({
-      newTodo: '',
-      todoList: this.state.todoList
+    }
+    TodoModel.create(newTodo, (id) => {
+      newTodo.id = id
+      this.state.todoList.push(newTodo)
+      this.setState({
+        newTodo: '',
+        todoList: this.state.todoList
+      })
+    }, (error) => {
+      console.log(error)
     })
   }
-
-  delete(event, todo) {
-    todo.deleted = true
-    this.setState(this.state)
+  delete(event, todo){
+    TodoModel.destroy(todo.id, () => {
+      todo.deleted = true
+      this.setState(this.state)
+    })
   }
 }
-
-
-
 
 export default App;
-
-let id = 0
-
-function idMaker(){
-  id = 1
-  return id
-}
