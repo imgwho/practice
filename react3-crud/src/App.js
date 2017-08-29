9b2b961f1c1b26fa4af39561343f9251732f4689
imgwho <imgwho@qq.com>
2017-08-29 22:34:35 +0800

    update: react task 3 deploy

--- react3-crud/src/App.js
+++ react3-crud/src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
             onChange = {this.changeTitle.bind(this)}
             onSubmit = {this.addTodo.bind(this)} />
         </div>
-      <ol>
+      <ol className="todoList">
         {todos}
       </ol>
       </div>
